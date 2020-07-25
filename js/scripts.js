592d406e6e707edc158c35aba3290917618bec69
Andrew Kind <andrew.w.kind@gmail.com>
2020-07-25 17:23:42 -0600

    Add delete and edit features.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 
+function deleteBlog() {
+
+}
+
 
 /**
  * Article Class
@@ -16,23 +20,128 @@ class Article {
    */
   output() {
 
-    const section = document.querySelector("section");
+    const section = document.querySelector(".blog-posts");
 
     let article = document.createElement("article");
+
 
     let paragraph = document.createElement("p");
     paragraph.innerText = `${this.title}`;
 
+
+    // Create Delete 
+    let deleteText = document.createElement("div");
+    deleteText.innerText = " [delete]";
+    deleteText.classList.add("delete");
+
+    // Create container for delete and edit
+    let detailsContainer = document.createElement("div");
+    detailsContainer.classList.add("details-container");
+
+
+    let date = document.createElement("div");
+    date.classList.add("date");
+
+    var d = new Date().toLocaleString();
+
+    date.innerText = d;
+
+    // When Delete is clicked, delete the item
+    deleteText.addEventListener("click", function () {
+
+      //delete this item
+      let item = this.parentElement.parentElement;
+
+      // Get label text. We need this to remove from the tasks array.
+      //var text = item.childNodes[3].innerText;
+
+      item.remove();
+
+      // TODO: remove from articles array
+    });
+
+
+
+    // Create Edit 
+
+    // When edit is clicked, begin editing
+    paragraph.addEventListener("click", function () {
+
+      //source: https://www.w3schools.com/jsref/tryit.asp?filename=try_dom_body_contenteditable
+
+      // Make the label editable
+      this.contentEditable = true;
+
+      // Remove its For attr so we can edit the text without conflict
+      this.classList.add("editable");
+
+      this.focus();
+
+    });
+
+
+    paragraph.addEventListener("blur", function () {
+
+      // remove style
+      this.classList.remove("editable");
+
+      // remove editable attr
+      this.contentEditable = false;
+
+    });
+
+    // Add to Paragraph
+    //paragraph.appendChild(deleteText);
+    //paragraph.appendChild(editText);
+
+    // Create content 
     let small = document.createElement("small");
     small.innerText = `${this.content}`;
 
+    // make content editable
+    // When edit is clicked, begin editing
+    small.addEventListener("click", function () {
+
+      //source: https://www.w3schools.com/jsref/tryit.asp?filename=try_dom_body_contenteditable
+
+      // Make the label editable
+      this.contentEditable = true;
+
+      // Remove its For attr so we can edit the text without conflict
+      this.classList.add("editable");
+
+      this.focus();
+    });
+
+
+    small.addEventListener("blur", function () {
+
+      // remove style
+      this.classList.remove("editable");
+
+      // remove editable attr
+      this.contentEditable = false;
+
+    });
+
+    // Add title and content to article element
+
+    detailsContainer.appendChild(deleteText);
+    detailsContainer.appendChild(date);
+
+
     article.appendChild(paragraph);
+
+    article.appendChild(detailsContainer);
+
     article.appendChild(small);
 
+    // Add article (blog post) to the section (blog post holder)
     section.appendChild(article);
     // Logic to add to website html
 
   }
+
 
 }
 
@@ -63,5 +172,21 @@ let articles = [
 
 for (article of articles) {
 
+  article.output();
+}
+
+let submitButton = document.querySelector(".submit-button");
+submitButton.addEventListener("click", addClick);
+
+function addClick(e) {
+
+  e.preventDefault();
+  let title = document.querySelector(".new-blog-title").value;
+  let content = document.querySelector(".new-blog-content").value;
+
+  let article = new Article(title, content);
+
+  console.log(article);
+  articles.push(article);
   article.output();
 }
